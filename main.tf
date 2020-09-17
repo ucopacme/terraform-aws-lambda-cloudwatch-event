1ebe87591476b2517269dfd210d42f3c3a15bba8
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2020-09-17 11:18:42 -0700

    updated the variables

--- main.tf
+++ main.tf
@@ -26,20 +26,23 @@ data "archive_file" "DataSource" {
 
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_Fun_StopEC2" {
+  count         = var.enabled ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.*.arn[0]
+  function_name = aws_lambda_function.this.arn
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.CloudWatch.arn
 
 }
 
 resource "aws_cloudwatch_event_rule" "CloudWatch" {
+  count         = var.enabled ? 1 : 0
   name        = var.function_name
   description = var.description
   schedule_expression = var.schedule_expression
 }
 resource "aws_cloudwatch_event_target" "lambda_schedular_target" {
+  count         = var.enabled ? 1 : 0
   rule = aws_cloudwatch_event_rule.CloudWatch.name
-  arn  = aws_lambda_function.this.*.arn[0]
+  arn  = aws_lambda_function.this.arn
 }
